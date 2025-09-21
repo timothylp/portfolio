02bd408d4ce569fbafaa308a6fa5eb6d0f4345bd
timothylp <timothylepallec@gmail.com>
2025-09-21 18:09:57 +0200

    Suppression page concurrence

--- app/concurrence-mariol-nova/asana-time-chart/index.tsx
+++
@@ -1,7 +0,0 @@
-import { queryAsanaTimeEntries } from "../asana";
-import { AsanaTimeChartClient } from "./index.client";
-
-export async function AsanaTimeChart({ excludedMonths = [] }: { excludedMonths?: string[] } = {}) {
-	const asanaTimeEntries = await queryAsanaTimeEntries();
-	return <AsanaTimeChartClient asanaTimeEntries={asanaTimeEntries} excludedMonths={excludedMonths} />;
-}
--- app/concurrence-mariol-nova/contributions-years-chart/index.tsx
+++
@@ -1,7 +0,0 @@
-import { queryUserContributionsByMonth } from "../gitlab";
-import { ContributionsYearsChartClient } from "./index.client";
-
-export async function ContributionsYearsChart() {
-	const contributionsByMonth = await queryUserContributionsByMonth();
-	return <ContributionsYearsChartClient contributionsByMonth={contributionsByMonth} />;
-}
